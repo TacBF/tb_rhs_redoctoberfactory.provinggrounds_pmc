7a3704ebf50aa598b2450a77641840c208be0ced
Ballista Milsim <ballista.milsim@gmail.com>
2017-04-17 16:45:58 +0200

    tb_rhs_redoctoberfactory.provinggrounds_pmc mission added

---
+++ briefing_blue.hpp
@@ -0,0 +1,5 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+
+
+<titleFont> Rise and go forth. Take what is rightfully ours. The dark side is with you.</titleFont><br />
+
---
+++ factions/BLU_F_roleRatio.hpp
@@ -0,0 +1,17 @@
+{
+	// {id, "caption", teamRatio, minimum, minTeamSize, minSquadSize, squadRatio}
+	{__ICE_gear_Rifleman, "Rifleman", __unlimited, 0, 0, 0, 0},
+	{__ICE_gear_Grenadier, "Grenadier", 8, 0, 0, 3, 0},
+	{__ICE_gear_MG, "MG", 4, 0, 0, 3, 0},
+	{__ICE_gear_Medic, "Medic", 4, 1, 0, 3, 0},
+{__ICE_gear_CombatEngineer, "Combat Engineer",0, 0, 0, 3, 0},
+{__ICE_gear_LightAT, "Medium AT",0  , 0, 0, 3, 0},
+{__ICE_gear_HeavyAT, "Heavy AT", 0 , 0, 0, 3, 0},
+{__ICE_gear_AA, "Anti-Air", 0 , 0, 0, 3, 0},
+	{__ICE_gear_Marksman, "Marksman", 8, 0, 0, 3, 0},
+	{__ICE_gear_Sniper, "Sniper", 10, 0, 0, 2, 0},
+	{__ICE_gear_SL, "Squad Leader", __unlimited, 0, 0, 1, 0},
+{__ICE_gear_Crewman, "Crewman",0 , 0, 0, 0, 0},
+{__ICE_gear_Pilot, "Pilot",0 , 0, 0, 0, 0},
+	{__ICE_gear_Diver, " ", 0, 0, 0, 0, 0}
+},
